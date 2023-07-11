5f999ecdcf2bc07006111ba2009103b29bfd96ee
Aleksander <afonia.marcii@gmail.com>
2023-07-11 19:21:39 +0300

    feat: add rooms section for home

---
+++ src/components/home/RoomsSection.vue
@@ -0,0 +1,80 @@
+<template>
+  <section>
+    <h2>Browse The Range</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <div class="room-cards">
+      <div class="room-card">
+        <img
+          src="../../assets/image/home/room-dining.png"
+          alt="dining room"
+          class="room-card__img"
+        />
+        <h3>Dining</h3>
+      </div>
+      <div class="room-card">
+        <img
+          src="../../assets/image/home/room-living.png"
+          alt="living room"
+          class="room-card__img"
+        />
+        <h3>Living</h3>
+      </div>
+      <div class="room-card">
+        <img
+          src="../../assets/image/home/room-bedroom.png"
+          alt="bedroom"
+          class="room-card__img"
+        />
+        <h3>Bedroom</h3>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+section {
+  /* height: 72rem; */
+  height: auto;
+  padding: 5.2rem 10rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+h2 {
+  color: #333;
+  font-size: 3.2rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  padding-right: 5.5rem;
+}
+p {
+  color: #666;
+  text-align: center;
+  font-size: 2rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  padding-left: 1.7rem;
+  padding-bottom: 6rem;
+}
+.room-cards {
+  display: flex;
+  flex-direction: row;
+  gap: 2rem;
+  justify-content: center;
+}
+.room-card__img {
+  border-radius: 15px;
+}
+h3 {
+  color: #333;
+  text-align: center;
+  font-size: 2.4rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  margin-top: 2.6rem;
+}
+</style>
--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,13 +1,16 @@
 <template>
   <PromoSection></PromoSection>
+  <RoomsSection></RoomsSection>
 </template>
 
 <script>
 import PromoSection from "../../components/home/PromoSection.vue";
+import RoomsSection from "../../components/home/RoomsSection.vue";
 
 export default {
   components: {
     PromoSection,
+    RoomsSection,
   },
 };
 </script>
