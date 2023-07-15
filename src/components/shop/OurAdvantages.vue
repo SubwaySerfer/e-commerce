8fc945a40b8c43465667872e5d06477d7d38e973
Aleksander <afonia.marcii@gmail.com>
2023-07-15 16:09:27 +0300

    feat: add advantages section

---
+++ src/components/shop/OurAdvantages.vue
@@ -0,0 +1,78 @@
+<template>
+  <section>
+    <div class="adv-card">
+      <img
+        src="src/assets/icons/shop/advantages/shop-trophy.svg"
+        alt="trophy icon"
+      />
+      <div>
+        <h3>High Quality</h3>
+        <p>crafted from top materials</p>
+      </div>
+    </div>
+    <div class="adv-card">
+      <img
+        src="src/assets/icons/shop/advantages/shop-guarantee.svg"
+        alt="guarantee icon"
+      />
+      <div>
+        <h3>Warranty Protection</h3>
+        <p>Over 2 years</p>
+      </div>
+    </div>
+    <div class="adv-card">
+      <img
+        src="src/assets/icons/shop/advantages/shop-shipping.svg"
+        alt="shipping icon"
+      />
+      <div>
+        <h3>Free Shipping</h3>
+        <p>Order over 150 $</p>
+      </div>
+    </div>
+    <div class="adv-card">
+      <img
+        src="src/assets/icons/shop/advantages/shop-support.svg"
+        alt="support icon"
+      />
+      <div>
+        <h3>24 / 7 Support</h3>
+        <p>Dedicated support</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+section {
+  height: 27rem;
+  background-color: #faf3ea;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10rem 5.3rem;
+}
+.adv-card {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+}
+h3 {
+  /* color: #242424;
+  font-family: Poppins; */
+  font-size: 2.5rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 150%;
+  text-align: start;
+}
+p {
+  color: #898989;
+  font-size: 2rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 150%;
+  text-align: start;
+}
+</style>
--- src/pages/shop/ShopPage.vue
+++ src/pages/shop/ShopPage.vue
@@ -1,13 +1,20 @@
 <template>
   <PromoShop></PromoShop>
   <FiltersShop></FiltersShop>
+
+  <OurAdvantages></OurAdvantages>
 </template>
 
 <script>
 import PromoShop from "../../components/shop/PromoShop.vue";
 import FiltersShop from "../../components/shop/FiltersShop.vue";
+import OurAdvantages from "../../components/shop/OurAdvantages.vue";
 
 export default {
-  components: { PromoShop, FiltersShop },
+  components: {
+    PromoShop,
+    FiltersShop,
+    OurAdvantages,
+  },
 };
 </script>
