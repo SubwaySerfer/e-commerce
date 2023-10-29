98e9626a74c627daa59f1338fb46f6286f4e3898
Aleksander Afanasyev <afonia.marcii@gmail.com>
2023-10-29 12:09:59 +0300

    update: fin postsTemplate

--- src/components/blog/PostsSection.vue
+++ src/components/blog/PostsSection.vue
@@ -5,6 +5,9 @@
     postImageAlt="work atmosphere, laptop, phone and nodepad."
     postDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum."
     postTitle="Going all-in with millennial design"
+    postOwner="Admin"
+    postDate="14 Oct 2022"
+    postMaterial="Handmade"
   ></post-template>
 </template>
 
