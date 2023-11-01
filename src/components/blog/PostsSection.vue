87f3fd7ea02f8301739c6bd8b620659628f44613
Aleksander Afanasyev <afonia.marcii@gmail.com>
2023-11-01 17:35:51 +0300

    fix: path

--- src/components/blog/PostsSection.vue
+++ src/components/blog/PostsSection.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="posts-wrapper">
     <post-template
-      postImage="/src/assets/image/posts/postImage-1.png"
+      postImage="assets/postImage-1.png"
       postImageAlt="work atmosphere, laptop, phone and nodepad."
       postDescription="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum."
       postTitle="Going all-in with millennial design"
