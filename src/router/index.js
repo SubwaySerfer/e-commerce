2e31b3170ae3521542816d205cf054869620a397
Aleksander Afanasyev <afonia.marcii@gmail.com>
2023-11-13 20:18:22 +0300

    feat: Cart Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,42 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
-import HomePage from '../pages/home/HomePage.vue'
-import ShopPage from '../pages/shop/ShopPage.vue'
-import ContactPage from '../pages/contact/ContactPage.vue'
-import BlogPage from '../pages/blog/BlogPage.vue'
-import CardInfoPage from '../pages/cardInfo/CardInfoPage.vue'
+import HomePage from '../pages/home/HomePage.vue';
+import ShopPage from '../pages/shop/ShopPage.vue';
+import ContactPage from '../pages/contact/ContactPage.vue';
+import BlogPage from '../pages/blog/BlogPage.vue';
+import CardInfoPage from '../pages/cardInfo/CardInfoPage.vue';
+import CartPage from '../pages/cart/CartPage.vue'
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{
-    path: '/',
-    redirect: '/home'
-  },
-{ 
-  path: '/home',
-  name: 'home',
-  component: HomePage },
-{ 
-  path:'/shop',
-  name:'shop',
-  component: ShopPage},
-  {
-    path: '/card-info',
-    name: 'cardInfo',
-    component: CardInfoPage
-  
-},
-{ path:'/contact',
-  name: 'contact',
-  component: ContactPage  
-},
-{ path: '/blog',
-  name:'blog',
-  component: BlogPage
-},
-]
-})
+  routes: [
+    {
+      path: '/',
+      redirect: '/home',
+    },
+    {
+      path: '/home',
+      name: 'home',
+      component: HomePage,
+    },
+    {
+      path: '/shop',
+      name: 'shop',
+      component: ShopPage,
+    },
+    {
+      path: '/card-info',
+      name: 'cardInfo',
+      component: CardInfoPage,
+    },
+    {
+      path: '/cart',
+      name: 'cart',
+      component: CartPage,
+    },
+    { path: '/contact', name: 'contact', component: ContactPage },
+    { path: '/blog', name: 'blog', component: BlogPage },
+  ],
+});
 
 export default router;
