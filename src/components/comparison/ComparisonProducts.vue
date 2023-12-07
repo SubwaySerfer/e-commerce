2a290f2bcee47e4d883a40c8abd7757afaa6df7e
Aleksander Afanasyev <afonia.marcii@gmail.com>
2023-12-07 22:54:32 +0300

    update: create compare table

--- src/components/comparison/ComparisonProducts.vue
+++ src/components/comparison/ComparisonProducts.vue
@@ -15,19 +15,56 @@
         </select>
       </div>
     </div>
+    <div class="comparising__main">
+      <h4 class="main__title__general">General</h4>
+      <ul class="main__items-box main__items-box__general">
+        <li class="items-box__li">
+          <h6>Sales Package</h6>
+          <p class="second_tb">test</p>
+          <p class="third_tb">test3</p>
+        </li>
+        <li class="items-box__li">
+          <h6>Model Number</h6>
+          <p class="second_tb"></p>
+          <p class="third_tb"></p>
+        </li>
+        <li class="items-box__li">
+          <h6>Secondary Material</h6>
+          <p class="second_tb"></p>
+          <p class="third_tb"></p>
+        </li>
+        <li class="items-box__li">
+          <h6>Configuration</h6>
+          <p class="second_tb"></p>
+          <p class="third_tb">yyy</p>
+        </li>
+        <li class="items-box__li">
+          <h6>Upholstery Material</h6>
+          <p class="second_tb"></p>
+          <p class="third_tb"></p>
+        </li>
+        <li class="items-box__li">
+          <h6>Upholstery Color</h6>
+          <p class="second_tb"></p>
+          <p class="third_tb"></p>
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 
 <style scoped>
 .comparison {
   width: 100%;
-  padding: 3.4rem;
+  padding: 0 5.4rem;
 }
 .comparison__promo {
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   width: 100%;
+  height: 38.6rem;
+  padding-top: 3.4rem;
 }
 .more__title {
   font-size: 2.8rem;
@@ -45,5 +82,51 @@
   width: 11.5rem;
 
   border-bottom: 2px solid #727272;
+}
+
+.comparising__main {
+  border-top: 1px solid #e8e8e8;
+  display: grid;
+  width: 100%;
+  grid-template-rows: 3rem 1fr 3rem 1fr 3rem 2fr;
+  grid-template-columns: repeat(4, 1fr);
+  color: #000;
+  font-family: Poppins;
+  font-size: 2rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 126.5%;
+  padding-top: 4.2rem;
+}
+.main__title__general {
+  grid-area: 1 / 1 / 2 / 2;
+  text-align: start;
+}
+.main__items-box {
+  list-style-type: none;
+  display: flex;
+  flex-direction: column;
+  row-gap: 3.4rem;
+  /* grid-area: 1 / 1 / 2 / 4; */
+  /* grid-area: 2 / 1 / 2 / 4; */
+}
+.main__items-box__general {
+  grid-area: 2 / 1 / 2 / 4;
+}
+
+.items-box__li > h6 {
+  text-align: start;
+}
+.items-box__li {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 100%;
+}
+.second_tb {
+  /* display: grid; */
+  grid-column: 2 / 3;
+}
+.third_tb {
+  grid-column: 3 / 4;
 }
 </style>
