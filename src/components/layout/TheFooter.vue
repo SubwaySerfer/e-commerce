f159d7780e03dd451b77088f4e73cd8c1c67086e
Aleksander Afanasyev <afonia.marcii@gmail.com>
2023-12-21 21:58:57 +0300

    update: media for footer

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -1,23 +1,27 @@
 <template>
-  <section>
-    <div class="footer">
-      <div>
-        <h4>Funiro.</h4>
-        <p class="adress-info">
+  <section class="footer">
+    <div class="footer__main-block">
+      <div class="contact-info">
+        <h4 class="contact-info__label">Funiro.</h4>
+        <p class="contact-info__adress">
           400 University Drive Suite 200 Coral Gables,<br />
           FL 33134 USA
         </p>
       </div>
       <div class="footer-nav">
-        <ul class="footer-nav-items footer-ul_first">
-          <li><h6>Links</h6></li>
+        <ul class="footer-nav-items footer-ul__first">
+          <li>
+            <h6 class="list__label">Links</h6>
+          </li>
           <li class="btn-root_vertical"><a href="/">Home</a></li>
           <li class="btn-root_vertical"><a href="/shop">Shop</a></li>
           <li class="btn-root_vertical"><a href="/blog">Blog</a></li>
           <li class="btn-root_vertical"><a href="/contact">Contact</a></li>
         </ul>
-        <ul class="footer-nav-items footer-ul_sec">
-          <li><h6>Help</h6></li>
+        <ul class="footer-nav-items footer-ul__sec">
+          <li>
+            <h6 class="list__label">Help</h6>
+          </li>
           <li class="btn-root_vertical"><a href="">Payment Options</a></li>
           <li class="btn-root_vertical"><a href="">Returns</a></li>
           <li class="btn-root_vertical"><a href="">Privacy Policies</a></li>
@@ -33,51 +37,56 @@
         </div>
       </div>
     </div>
-    <div>
+    <div class="footer__rights-block">
       <p class="rights-info">2023 furino. All rights reverved</p>
     </div>
   </section>
 </template>
 
 <style scoped>
-h4 {
+.contact-info__label {
   font-size: 2.4rem;
   font-style: normal;
   font-weight: 700;
   line-height: normal;
   text-align: start;
 }
-section {
+
+.footer {
   height: 50.5rem;
   padding: 4.7rem 10rem 3.8rem;
   border-top: 0.1rem solid #d9d9d9;
 }
-.footer {
+
+.footer__main-block {
   display: flex;
   flex-direction: row;
   justify-content: flex-start;
   gap: 13.9rem;
   padding-bottom: 4.8rem;
 }
+
 .footer-nav {
   display: flex;
   flex-direction: row;
   justify-content: flex-start;
-  /* gap: 14.6rem; */
   width: 100%;
-  /* padding-left: 1px; */
 }
+
 .footer-nav-items {
   display: flex;
   flex-direction: column;
   gap: 4.6rem;
 }
+
 .footer-ul_first {
   margin-right: 14.6rem;
 }
+
 .footer-ul_sec {
   margin-right: 7.2rem;
 }
+
 a {
   color: #000;
   font-size: 1.6rem;
@@ -88,7 +97,8 @@ a {
   text-align: start;
   text-decoration: none;
 }
-h6,
+
+.list__label,
 .input-box_label {
   color: #9f9f9f;
   font-size: 1.6rem;
@@ -99,7 +109,8 @@ h6,
   margin: 0;
   margin-bottom: 0.9rem;
 }
-.adress-info {
+
+.contact-info__adress {
   color: #9f9f9f;
   font-size: 1.6rem;
   font-style: normal;
@@ -110,14 +121,17 @@ h6,
   padding-top: 5rem;
   padding-left: 0.2rem;
 }
+
 li {
   list-style-type: none;
   align-items: flex-start;
 }
+
 ul {
   margin: 0;
   padding: 0;
 }
+
 .rights-info {
   font-size: 1.6rem;
   font-style: normal;
@@ -129,16 +143,19 @@ ul {
   padding-top: 3.4rem;
   padding-left: 0.2rem;
 }
+
 .input-box {
   display: flex;
   flex-direction: column;
   gap: 4.6rem;
 }
+
 .input-box__content {
   gap: 1.1rem;
   display: flex;
   flex-direction: row;
 }
+
 input {
   border: none;
   border-bottom: 0.1rem solid #000;
@@ -151,6 +168,7 @@ input {
   padding-bottom: 0.3rem;
   letter-spacing: 0.03em;
 }
+
 .button-news {
   background-color: transparent;
   border: 0;
@@ -162,11 +180,100 @@ input {
   line-height: normal;
   padding: 0 0 0.3rem 0;
 }
+
 .button-news_text {
   transition: all ease-in 0.25s;
   display: block;
 }
-.button-news:hover > .button-news_text:hover {
+
+.button-news:hover>.button-news_text:hover {
   transform: translateY(-2px);
+}
+
+@media (max-width: 1300px) {
+  .footer__main-block {
+    gap: 1rem;
+    justify-content: space-between;
+  }
+
+  .footer {
+    padding: 4.7rem 6.98vw 3.8rem;
+  }
+
+  .footer-nav {
+    justify-content: space-evenly;
+  }
+
+  .footer-ul__first {
+    margin-right: 0;
+  }
+
+  .footer-ul__sec {
+    margin-right: 0;
+  }
+}
+
+@media(max-width: 1000px) {
+  .footer-nav {
+    justify-content: space-between;
+    gap: 1rem;
+  }
+
+  .footer {
+    padding: 4.7rem 3vw 3.8rem;
+  }
+}
+
+@media(max-width: 870px) {
+  .footer__main-block {
+    flex-direction: column;
+    height: auto;
+    align-items: center
+  }
+
+  .contact-info {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 90vw;
+    margin-bottom: 2rem;
+  }
+
+  .contact-info__adress {
+    padding-top: 0;
+  }
+
+  .footer__rights-block {
+    padding-bottom: 3rem;
+  }
+
+}
+
+@media (max-width: 603px) {
+  .input-box__content {
+    flex-direction: column;
+    align-items: center
+  }
+
+  .button-news {
+    width: min-content;
+  }
+}
+
+@media (max-width: 470px) {
+  .footer-nav {
+    flex-direction: column;
+    align-items: center;
+    width: 80vw;
+    gap: 3rem;
+  }
+
+  .footer-nav-items {
+    align-items: center
+  }
+
+  .input-box {
+    align-items: center
+  }
 }
 </style>
